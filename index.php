9d11a7b7c9394d2590096f1fba3d0bd6e1154d99
Carlos Ramon Moreira Leite <e.carlos.leite@mpt.mp.br>
2019-08-30 12:19:20 -0400

    greeting de logon

--- index.php
+++ index.php
@@ -35,17 +35,40 @@
                         </div>        
                     </a>                                            
                 </div>
-                <div id="greeting" class="none">
+                <!-- <div id="greeting" class="none">
                     <span>Bem-vindo !</span>
                     <span id="logout">| clique <a href="./src/logout.php">AQUI</a> para sair</span>
+                </div> -->
+                <div id="greeting" class="none">
+                    <span>Bem-vindo</span>
                 </div>
+                <div id="btn_login" class="btn_bar">
+                    <span id="logout">
+                        | clique 
+                        <a href="./src/logout.php">
+                            AQUI
+                        </a>
+                         para sair.
+                    </span>
+                </div>
+                <a href="auth.html">
+                    <div id="btn_login" class="btn_bar">
+                        <span>Login</span>
+                    </div>";
+                </a>
                 <?php
                 session_start();
 
-                if(isset($_SESSION["nome_usuario"])){
+                if(!isset($_SESSION["nome_usuario"])){
                     echo "<div id=\"greeting\" class=\"none\">";
-                    echo "    <span>Bem-vindo ".$_SESSION["nome_usuario"]." !</span>";
-                    echo "    <span id=\"logout\">| clique <a href=\"./src/logout.php\">AQUI</a> para sair</span>";
+                    echo "    <span>Bem-vindo ";
+                    if(isset($_SESSION["nome_usuario"])){
+                        echo $_SESSION["nome_usuario"];
+                    }
+                    echo "    </span>";
+                    echo "</div>";
+                    echo "<div id=\"btn_login\" class=\"btn_bar\">";
+                    echo "    <span id=\"logout\">| clique <a href=\"./src/logout.php\">AQUI</a> para sair.</span>";
                     echo "</div>";
                 } else {
                     echo "<a href=\"auth.html\">";
