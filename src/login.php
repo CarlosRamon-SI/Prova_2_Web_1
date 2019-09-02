5d886cbafd06a61cd0e16e93b20d1eb73fd471bf
Carlos Ramon Moreira Leite <e.carlos.leite@mpt.mp.br>
2019-09-02 11:53:53 -0400

    caminhos ***

--- src/login.php
+++ src/login.php
@@ -14,12 +14,12 @@ if($total) {
     $dados = pg_fetch_array($result_id); 
     if(!strcmp($senha, $dados["senha"])) {
         $_SESSION["nome_usuario"] = stripslashes($dados["usuario"]); 
-        header("Location: /index.php");
+        header("Location: ../index.php");
         unset($_SESSION["logon"]);
         exit; 
     }
 }
 $_SESSION["logon"] = "Login ou Senha inválidos!";
-header("Location: /index.php");
+header("Location: ../index.php");
 exit;
 ?>
