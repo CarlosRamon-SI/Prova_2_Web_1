47d382f4c8ee72c7eaec70d3d8f4ec984f50cd26
Carlos Ramon Moreira Leite <e.carlos.leite@mpt.mp.br>
2019-09-02 10:35:25 -0400

    greetings semi-pronto

--- index.php
+++ index.php
@@ -34,51 +34,22 @@
                             <span>Btn 05</span>
                         </div>        
                     </a>                                            
+                    <div id="none" class="greeting">
+                        <span>Bem-vindo</span>
+                        <span id="logout">
+                            | clique 
+                            <a href="./src/logout.php">
+                                AQUI
+                            </a>
+                             para sair.
+                        </span>
+                    </div>
+                    <!-- <a href="auth.html">
+                        <div id="btn_login" class="btn_bar">
+                            <span>Login</span>
+                        </div>
+                    </a> -->
                 </div>
-                <!-- <div id="greeting" class="none">
-                    <span>Bem-vindo !</span>
-                    <span id="logout">| clique <a href="./src/logout.php">AQUI</a> para sair</span>
-                </div> -->
-                <div id="greeting" class="none">
-                    <span>Bem-vindo</span>
-                </div>
-                <div id="btn_login" class="btn_bar">
-                    <span id="logout">
-                        | clique 
-                        <a href="./src/logout.php">
-                            AQUI
-                        </a>
-                         para sair.
-                    </span>
-                </div>
-                <a href="auth.html">
-                    <div id="btn_login" class="btn_bar">
-                        <span>Login</span>
-                    </div>";
-                </a>
-                <?php
-                session_start();
-
-                if(!isset($_SESSION["nome_usuario"])){
-                    echo "<div id=\"greeting\" class=\"none\">";
-                    echo "    <span>Bem-vindo ";
-                    if(isset($_SESSION["nome_usuario"])){
-                        echo $_SESSION["nome_usuario"];
-                    }
-                    echo "    </span>";
-                    echo "</div>";
-                    echo "<div id=\"btn_login\" class=\"btn_bar\">";
-                    echo "    <span id=\"logout\">| clique <a href=\"./src/logout.php\">AQUI</a> para sair.</span>";
-                    echo "</div>";
-                } else {
-                    echo "<a href=\"auth.html\">";
-                    echo "    <div id=\"btn_login\" class=\"btn_bar\">";
-                    echo "        <span>Login</span>";
-                    echo "    </div>";
-                    echo "</a>";
-                }
-
-                ?>
             </div>
         </div>
         <div id="corpo">
