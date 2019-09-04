c376749079518bfef5a60cf32414bd0fe3a22f09
Carlos Ramon Moreira Leite <e.carlos.leite@mpt.mp.br>
2019-09-04 08:38:15 -0400

    redefinições

--- sandbox.php
+++ sandbox.php
@@ -1,44 +1,44 @@
 <?php
-                session_start();
+session_start();
 
-                if(!isset($_SESSION["nome_usuario"])){
-                    echo "<div id=\"greeting\" class=\"none\">";
-                    echo "    <span>Bem-vindo ";
-                    if(isset($_SESSION["nome_usuario"])){
-                        echo $_SESSION["nome_usuario"];
-                    }
-                    echo "    </span>";
-                    echo "</div>";
-                    echo "<div id=\"btn_login\" class=\"btn_bar\">";
-                    echo "    <span id=\"logout\">| clique <a href=\"./src/logout.php\">AQUI</a> para sair.</span>";
-                    echo "</div>";
-                } else {
-                    echo "<a href=\"auth.html\">";
-                    echo "    <div id=\"btn_login\" class=\"btn_bar\">";
-                    echo "        <span>Login</span>";
-                    echo "    </div>";
-                    echo "</a>";
-                }
+if(!isset($_SESSION["nome_usuario"])){
+    echo "<div id=\"greeting\" class=\"none\">";
+    echo "    <span>Bem-vindo ";
+    if(isset($_SESSION["nome_usuario"])){
+        echo $_SESSION["nome_usuario"];
+    }
+    echo "    </span>";
+    echo "</div>";
+    echo "<div id=\"btn_login\" class=\"btn_bar\">";
+    echo "    <span id=\"logout\">| clique <a href=\"./src/logout.php\">AQUI</a> para sair.</span>";
+    echo "</div>";
+} else {
+    echo "<a href=\"auth.html\">";
+    echo "    <div id=\"btn_login\" class=\"btn_bar\">";
+    echo "        <span>Login</span>";
+    echo "    </div>";
+    echo "</a>";
+}
 
-            if(!isset($_SESSION["nome_usuario"])){
-                echo    "<div id=\"none\" class=\"greeting\">";
-                echo    "    <span>Bem-vindo ";
-                if(isset($_SESSION["nome_usuario"])){
-                    echo $_SESSION["nome_usuario"] . "</span>";
-                }
-                echo    "    <span id=\"logout\">";
-                echo    "        | clique ";
-                echo    "        <a href=\"./src/logout.php\">";
-                echo    "            AQUI";
-                echo    "        </a>";
-                echo    "         para sair.";
-                echo    "    </span>";
-                echo    "</div>";
-            } else {
-                echo    "<a href=\"auth.html\">";
-                echo    "    <div id=\"btn_login\" class=\"btn_bar\">";
-                echo    "</a>";
-            }
+if(!isset($_SESSION["nome_usuario"])){
+    echo    "<div id=\"none\" class=\"greeting\">";
+    echo    "    <span>Bem-vindo ";
+    if(isset($_SESSION["nome_usuario"])){
+        echo $_SESSION["nome_usuario"] . "</span>";
+    }
+    echo    "    <span id=\"logout\">";
+    echo    "        | clique ";
+    echo    "        <a href=\"./src/logout.php\">";
+    echo    "            AQUI";
+    echo    "        </a>";
+    echo    "         para sair.";
+    echo    "    </span>";
+    echo    "</div>";
+} else {
+    echo    "<a href=\"auth.html\">";
+    echo    "    <div id=\"btn_login\" class=\"btn_bar\">";
+    echo    "</a>";
+}
 
-                ?>
+?>
                         
