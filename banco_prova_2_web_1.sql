1d6e267f7270f4f0c15e8353ce0f60a73b7df7e5
Carlos Ramon Moreira Leite <ramon.ethan@gmail.com>
2019-09-06 11:13:46 -0400

    up

--- banco_prova_2_web_1.sql
+++ banco_prova_2_web_1.sql
@@ -5,8 +5,8 @@ CREATE DATABASE carlos3;
 CREATE TABLE usuarios (
     id serial primary key not null,
     usuario varchar(120),
-    senha varchar(220),
-    nome varchar(120)
+    nome varchar(120),
+    senha varchar(220)
 );
 
 INSERT INTO usuarios(usuario,nome,senha) VALUES('admin','jivago','21232f297a57a5a743894a0e4a801fc3'); -- senha admin
